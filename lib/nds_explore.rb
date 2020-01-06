8b70887958ef0bd1819692931844c2b735db27da
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-06 00:58:20 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,5 +10,12 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+first_dir_movie = directors_database[0][:movies]
+index = 0
 
+while index <first_dir_movie.length do
+  titles = ss_movies[index][:title}
+    puts titles
+    index += 1
+  end
 end
